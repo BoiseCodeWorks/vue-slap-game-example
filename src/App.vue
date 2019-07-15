8acc91277267c295047e9f01c562e8323795c6a2
Darryl Kilzer <darrylkilzer@gmail.com>
2019-07-15 15:56:45 -0600

    slapgame example

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,87 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <h1>stuff</h1>
+    <!-- <router-view /> -->
+    <Home :target="target" />
+    <Home :target="target2" />
   </div>
 </template>
 
+<script>
+  import Home from './views/Home.vue'
+  export default {
+    data() {
+      return {
+        target: {
+          name: "Battleship",
+          health: 100,
+          hits: 0,
+          attacks: [
+            {
+              name: "Slap",
+              dmg: -1
+            },
+            {
+              name: "Torpedo",
+              dmg: 10
+            },
+            {
+              name: "Nuke",
+              dmg: 1000000
+            }
+          ]
+        },
+        target2: {
+          name: "Batman",
+          health: 500,
+          hits: 0,
+          attacks: [
+            {
+              name: "Batarang",
+              dmg: -20
+            },
+            {
+              name: "Kick",
+              dmg: 10
+            },
+            {
+              name: "Weakness",
+              dmg: 0
+            }
+          ]
+        }
+      }
+    },
+    components: {
+      Home
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+  h1 {
+    color: orangered;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #nav {
+    padding: 30px;
+  }
+
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home row">
+    <div class="col-12">
+      <h1>{{target.name}}</h1>
+      <h3 :class="healthStatus()" v-if="target.health > 0">Health: {{target.health}}</h3>
+      <h3 v-if="target.health == 0">It's dead Jim</h3>
+      <h3>Hits: {{target.hits}}</h3>
+      <button class="btn btn-danger" @click="attackShip(attack)"
+        v-for="attack in target.attacks">{{attack.name}}</button>
+      <button @click="reset" class="btn btn-success">Reset</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home',
+    props: ["target"],
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      attackShip(atk) {
+        this.target.health -= atk.dmg
+        this.target.hits++
+        if (this.target.health < 0) {
+          this.target.health = 0
+        }
+      },
+      healthStatus() {
+        let output = "red"
+        if (this.target.health >= 90) {
+          output = 'green'
+        }
+        else if (this.target.health >= 70) {
+          output = 'yellow'
+        }
+        else if (this.target.health >= 50) {
+          output = 'orangered'
+        }
+        return output
+      },
+      reset() {
+        this.target.health = 100
+        this.target.hits = 0
+      }
+    }
   }
-}
 </script>
+
+
+<style>
+  .green {
+    color: green;
+  }
+
+  .red {
+    color: red;
+  }
+
+  .orangered {
+    color: orangered;
+  }
+
+  .yellow {
+    color: yellow;
+  }
+
+  h1 {
+    color: red;
+  }
+</style>
